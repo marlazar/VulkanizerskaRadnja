8e280b560254c212463aa16a923c262ef506cd4f
marlazar <lmarkovicbb@gmail.com>
2018-03-15 12:21:58 +0100

    Generisane metode equals, hashCode i toString.

--- Menjacnica/src/menjacnica/Kurs.java
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -32,4 +32,45 @@ public class Kurs {
 		this.vrednost = vrednost;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(vrednost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((vrsta == null) ? 0 : vrsta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(vrednost) != Double.doubleToLongBits(other.vrednost))
+			return false;
+		if (vrsta == null) {
+			if (other.vrsta != null)
+				return false;
+		} else if (!vrsta.equals(other.vrsta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Kurs [vrsta=" + vrsta + ", datum=" + datum + ", vrednost=" + vrednost + "]";
+	}
+
 }
