64d6eb3f708abce13489058941e3e71e45da9b2d
marlazar <lmarkovicbb@gmail.com>
2018-03-15 13:22:24 +0100

    Izmenjena metoda dodajKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,15 +1,29 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+
 	@Override
 	public void dodajKurs(String valuta, String vrsta, double kurs, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
+		Valuta valu = new Valuta();
+		valu.setNaziv(valuta);
+		Kurs k = new Kurs();
+		k.setDatum(datum);
+		k.setVrsta(vrsta);
+		k.setVrednost(kurs);
 
+		if (valute.contains(valu))
+			throw new RuntimeException("Valuta vec postoji u bazi!");
+		int obj = valute.indexOf(valu);
+
+		valute.get(obj).getKursevi().add(k);
 	}
 
 	@Override
