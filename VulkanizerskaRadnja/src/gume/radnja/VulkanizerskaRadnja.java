a9256157673c1ca9248ab36c0500d4afd3b552c7
marlazar <lmarkovicbb@gmail.com>
2018-03-29 03:11:39 +0200

    Napisana JavaDoc za klasu VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -1,19 +1,44 @@
 package gume.radnja;
 
 import java.util.LinkedList;
+
 import gume.AutoGuma;
 
-public class VulkanizerskaRadnja {
-	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+/**
+ * Klasa koja sadrzi listu guma. Nove gume se mogu dodavati i moze se prolaziti kroz listu
+ * @author marlazar
+ * @version 1.0
+ *
+ */
+public class VulkanizerskaRadnja {
+	
+	/**
+	 * Lista objekata klase AutoGuma
+	 */
+	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
+	
+	/**
+	 * Klasa koja dodaje gumu u listu, ako objekat nije null ili vec postoji
+	 * @param a autoGuma
+	 * @throws java.lang.NullPointerException Baca exception ako je guma null
+	 * @throws java.lang.RuntimeException Baca ako guma vec postoji u listi
+	 */
+	
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
 		if (gume.contains(a))
 			throw new RuntimeException("Guma vec postoji");
-		gume.addFirst(a);
-	}
-
+			gume.addFirst(a);
+		}
+	
+	/**
+	 * Klasa koja pronalazi gumu u listi
+	 * @param markaModel vraca marku/model gume
+	 * @return novaLista lista objekata
+	 */
+	
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
@@ -23,4 +48,6 @@ public class VulkanizerskaRadnja {
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
+
+	
 }
